1198227a09d2b6310c8c3fb439fedb6087f72b62
Ari-Pekka Härkönen <ari-pekka.harkonen@aalto.fi>
2017-01-20 22:39:28 +0200

    main

--- Documents/ohjelmointistuff/Valaspeli/src/project/Main.scala
+++ Documents/ohjelmointistuff/Valaspeli/src/project/Main.scala
@@ -1,5 +1,50 @@
-package project
+import processing.core._
+import processing.core.PConstants._
+import scala.util.Random
+import scala.math._
+//import scala.swing.event.MousePressed
 
-class Main {
+class ScalaProcessingExample extends PApplet {
+
+ var yoff = 0.0f;        // 2nd dimension of perlin noise
+
+override def settings() {
+   size(1140, 680);
+}
   
+
+override def draw() {
+  background(0);
+
+  fill(194, 223, 255);
+  // We are going to draw a polygon out of the wave points
+  beginShape(); 
+  
+  var xoff = 0f;       // Option #1: 2D Noise
+  // float xoff = yoff; // Option #2: 1D Noise
+  
+  // Iterate over horizontal pixels
+  for (x <- 0 to width by 10) {
+    // Calculate a y value according to noise, map to 
+    var y = PApplet.map(noise(xoff, yoff), 0, 1, 200,300) // Option #1: 2D Noise
+    // float y = map(noise(xoff), 0, 1, 200,300);    // Option #2: 1D Noise
+    
+    // Set the vertex
+    vertex(x, y); 
+    // Increment x dimension for noise
+    xoff += 0.05f
+  }
+  // increment y dimension for noise
+  yoff += 0.005f
+  vertex(width, height);
+  vertex(1, height);
+  endShape(PConstants.CLOSE);
+}
+  
+}
+
+object ScalaProcessingExample {
+  def main(args: Array[String]) {
+    PApplet.main(Array[String]("ScalaProcessingExample"))
+  }
 }
